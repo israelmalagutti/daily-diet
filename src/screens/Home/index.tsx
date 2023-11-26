4102607b0148dab14e1624b4d5f65666a2fcfbeb
Israel Malagutti <israelmalagutti@gmail.com>
2023-11-25 23:52:37 -0300

    Screen - create Home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,18 @@
+import { Layout } from "@components/index";
+import { Header } from "@components/Header";
+
+// import { Container } from './styles';
+
+export function Home() {
+  return (
+    <Layout>
+      <Header />
+
+      {/* MealStatusCard */}
+
+      {/* Meals */}
+      {/* AddMealButton */}
+      {/* Flatlist MealCard */}
+    </Layout>
+  );
+}
