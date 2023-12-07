38bb8ad48d8d187019866803b273923cd5c2de2c
Israel Malagutti <israelmalagutti@gmail.com>
2023-12-07 17:11:52 -0300

    Screen - finalize screen

--- src/screens/Meal/FeedbackMeal/index.tsx
+++ src/screens/Meal/FeedbackMeal/index.tsx
@@ -1,9 +1,19 @@
 import { useNavigation, useRoute } from "@react-navigation/native";
 
-import { Layout } from "@components/index";
+import { Button, Layout } from "@components/index";
 import { type DietStyleProps } from "@components/DietPicker/styles";
 
-import { Container } from "./styles";
+import personHappy from "@assets/person/personHappy.png";
+import personUpset from "@assets/person/personUpset.png";
+
+import {
+  Diet,
+  Illustration,
+  StyleContainer,
+  Subtitle,
+  Title,
+  TitleContainer,
+} from "./styles";
 
 type RouteParams = {
   diet: DietStyleProps;
@@ -15,9 +25,45 @@ export function FeedbackMeal() {
   const route = useRoute();
   const { diet } = route.params as RouteParams;
 
+  const TITLE =
+    (diet === "WITHIN" && "Keep it up!") || (diet === "OFF" && "What a pitty!");
+
+  const SUBTITLE =
+    (diet === "WITHIN" && (
+      <Subtitle>
+        You <Diet>continue on the diet</Diet>. Very good!
+      </Subtitle>
+    )) ||
+    (diet === "OFF" && (
+      <Subtitle>
+        You're <Diet>off the diet</Diet> this time, but keep trying and don't
+        give up! Very good!
+      </Subtitle>
+    ));
+
+  const ILLUSTRATION =
+    (diet === "WITHIN" && personHappy) || (diet === "OFF" && personUpset);
+
+  const handle = () => {
+    navigation.reset({ routes: [{ name: "home" }] });
+  };
+
   return (
     <Layout>
-      <Container />
+      <StyleContainer>
+        <TitleContainer>
+          <Title diet={diet}>{TITLE}</Title>
+          {SUBTITLE}
+        </TitleContainer>
+
+        <Illustration alt="Illustration" source={ILLUSTRATION} />
+
+        <Button
+          type="SECONDARY"
+          text=" Go to the home screen"
+          onPress={handle}
+        />
+      </StyleContainer>
     </Layout>
   );
 }
--- src/screens/Meal/FeedbackMeal/styles.ts
+++ src/screens/Meal/FeedbackMeal/styles.ts
@@ -2,7 +2,11 @@ import styled, { css } from "styled-components/native";
 
 import { DietStyleProps } from "@components/DietPicker/styles";
 
-export const Container = styled.View`
+type TitleStyleProps = {
+  diet: DietStyleProps;
+};
+
+export const StyleContainer = styled.View`
   flex: 1;
 
   align-items: center;
@@ -10,4 +14,44 @@ export const Container = styled.View`
 
   padding: 0 0 104px;
   gap: 40px;
+`;
+
+export const TitleContainer = styled.View`
+  gap: 8px;
+`;
+
+export const Title = styled.Text<TitleStyleProps>`
+  ${({ theme, diet }) => css`
+    text-align: center;
+    color: ${(diet === "WITHIN" && theme.COLORS.GREEN_DARK) ||
+    (diet === "OFF" && theme.COLORS.RED_DARK)};
+
+    font-size: ${theme.FONT_SIZE.XL}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+  `};
+`;
+
+export const Subtitle = styled.Text`
+  ${({ theme }) => css`
+    text-align: center;
+    color: ${theme.COLORS.GRAY_700};
+
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+  `};
+`;
+
+export const Diet = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_700};
+
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+  `};
+`;
+
+export const Illustration = styled.Image``;
+
+export const ButtonContainer = styled.View`
+  padding: 0 48px;
 `;
