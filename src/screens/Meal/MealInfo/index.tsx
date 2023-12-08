85f1e56987d1b5ad10d4304e3fd34ac68af81991
Israel Malagutti <israelmalagutti@gmail.com>
2023-12-07 23:42:53 -0300

    Screen - finalize MealInfo

--- src/screens/Meal/MealInfo/index.tsx
+++ src/screens/Meal/MealInfo/index.tsx
@@ -1,28 +1,103 @@
-import { Text } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
 
-import { Header, Layout } from "@components/index";
+import { Button, Header, Layout, Modal } from "@components/index";
 import { type MealType } from "@components/Meal/MealCard";
 
-import { Container } from "./styles";
+import {
+  ActionsContainer,
+  DateTime,
+  DietContainer,
+  DietIndicator,
+  DietText,
+  ContentContainer,
+  Meal,
+  MealContainer,
+  InfoContainer,
+} from "./styles";
+import { format } from "date-fns";
+import { useState } from "react";
+import { deleteMeal } from "@storage/meals";
 
 type RouteParams = {
   meal: MealType;
 };
 
-export function MealInfo({}) {
+export function MealInfo() {
+  const [modalOpen, setModalOpen] = useState(false);
+
   const navigation = useNavigation();
 
   const route = useRoute();
   const { meal } = route.params as RouteParams;
 
-  return (
-    <Layout>
-      <Header backButton="DEFAULT" screenName="New meal" />
+  const date = new Date(meal.createdAt);
 
-      <Container>
-        <Text>MealInfo</Text>
-      </Container>
+  const year = format(date, "dd/MM/yyyy");
+  const time = format(date, "HH:ss");
+
+  const LAYOUT_BACKGROUND_COLOR =
+    meal.diet === "WITHIN" ? "GREEN_LIGHT" : "RED_LIGHT";
+
+  const DIET_TEXT = meal.diet === "WITHIN" ? "Within diet" : "Off diet";
+
+  const toggleModal = () => {
+    setModalOpen(prevState => !prevState);
+  };
+
+  const handleDelete = async (meal: MealType) => {
+    try {
+      await deleteMeal(meal);
+
+      navigation.navigate("home");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Layout bgColor={LAYOUT_BACKGROUND_COLOR}>
+      <Header backButton="DEFAULT" screenName="Meal" />
+
+      <ContentContainer>
+        <InfoContainer>
+          <MealContainer>
+            <Meal.Name numberOfLines={2}>{meal.name}</Meal.Name>
+
+            <Meal.Description>
+              {meal.description ?? "This meal doens't have a description"}
+            </Meal.Description>
+          </MealContainer>
+
+          <MealContainer>
+            <DateTime.Header>Date and time</DateTime.Header>
+            <DateTime.Content>{`${year} at ${time}`}</DateTime.Content>
+          </MealContainer>
+
+          <DietContainer>
+            <DietIndicator diet={meal.diet} />
+            <DietText>{DIET_TEXT}</DietText>
+          </DietContainer>
+        </InfoContainer>
+
+        <ActionsContainer>
+          <Button icon="edit" text="Edit meal" onPress={() => {}} />
+          <Button
+            type="SECONDARY"
+            icon="delete-forever"
+            text="Delete meal"
+            onPress={toggleModal}
+          />
+        </ActionsContainer>
+
+        <Modal
+          visible={modalOpen}
+          message="Are you sure you want to delete this meal?"
+          actions={[
+            { text: "Cancel", type: "SECONDARY", onPress: () => toggleModal() },
+            { text: "Delete", onPress: () => handleDelete(meal) },
+          ]}
+        />
+      </ContentContainer>
     </Layout>
   );
 }
--- src/screens/Meal/MealInfo/styles.ts
+++ src/screens/Meal/MealInfo/styles.ts
@@ -1,3 +1,123 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
-export const Container = styled.View``;
+import { type DietStyleProps } from "@components/DietPicker/styles";
+
+type DietProps = {
+  diet: DietStyleProps;
+};
+
+export const ContentContainer = styled.View`
+  ${({ theme }) => css`
+    background-color: ${theme.COLORS.GRAY_100};
+  `};
+
+  position: absolute;
+
+  height: 93%;
+
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  padding: 40px 24px 32px;
+
+  border-radius: 20px;
+`;
+
+export const InfoContainer = styled.View`
+  flex: 1;
+
+  gap: 24px;
+`;
+
+export const MealContainer = styled.View`
+  gap: 8px;
+`;
+
+const Name = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_700};
+
+    font-size: ${theme.FONT_SIZE.XL}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+  `}
+`;
+
+const Description = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_700};
+
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+  `}
+`;
+
+export const Meal = {
+  Name,
+  Description,
+};
+
+const Header = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_700};
+
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+  `}
+`;
+
+const Content = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_700};
+
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+  `}
+`;
+
+export const DateTime = {
+  Header,
+  Content,
+};
+
+export const DietContainer = styled.View`
+  ${({ theme }) => css`
+    background-color: ${theme.COLORS.GRAY_200};
+  `}
+  flex-direction: row;
+
+  align-self: flex-start;
+  align-items: center;
+  justify-content: center;
+
+  padding: 8px 12px;
+  gap: 8px;
+
+  border-radius: 999px;
+`;
+
+export const DietIndicator = styled.View<DietProps>`
+  ${({ theme, diet }) => css`
+    background-color: ${diet === "WITHIN"
+      ? theme.COLORS.GREEN_DARK
+      : theme.COLORS.RED_DARK};
+  `}
+
+  height: 8px;
+  width: 8px;
+
+  border-radius: 999px;
+`;
+
+export const DietText = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_700};
+
+    font-size: ${theme.FONT_SIZE.SM}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+  `}
+`;
+
+export const ActionsContainer = styled.View`
+  gap: 8px;
+`;
