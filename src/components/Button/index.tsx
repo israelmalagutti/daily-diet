68de8aeb17f086b901fb7e9a49211e5fcab9c722
Israel Malagutti <israelmalagutti@gmail.com>
2023-12-07 23:38:17 -0300

    Component - add a secondary type to Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,6 +7,7 @@ export type ButtonProps = TouchableOpacityProps & {
   text: string;
   icon?: keyof typeof MaterialIcons.glyphMap;
 
+  size?: ButtonTypeStyleProps;
   type?: ButtonTypeStyleProps;
   onPress?: () => void;
 };
@@ -14,15 +15,19 @@ export type ButtonProps = TouchableOpacityProps & {
 export function Button({
   icon,
   text,
+
+  size = "PRIMARY",
   type = "PRIMARY",
   onPress,
   ...rest
 }: ButtonProps) {
   return (
-    <Container {...rest} type={type} onPress={onPress}>
-      {!!icon && <Icon name={icon} size={18} />}
+    <Container {...rest} type={type} size={size} onPress={onPress}>
+      {!!icon && <Icon name={icon} size={18} type={type} />}
 
-      <Text>{text}</Text>
+      <Text type={type} size={size}>
+        {text}
+      </Text>
     </Container>
   );
 }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -5,30 +5,40 @@ export type ButtonTypeStyleProps = "PRIMARY" | "SECONDARY";
 
 type ButtonProps = {
   type: ButtonTypeStyleProps;
+  size: ButtonTypeStyleProps;
+};
+
+type IconProps = {
+  type: ButtonTypeStyleProps;
 };
 
 export const Container = styled.TouchableOpacity<ButtonProps>`
-  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
-  width: ${({ type }) => (type === "PRIMARY" ? "100%" : "auto")};
+  background-color: ${({ theme, type }) =>
+    type === "PRIMARY" ? theme.COLORS.GRAY_600 : "#ffffff00"};
+  width: ${({ size }) => (size === "PRIMARY" ? "100%" : "auto")};
 
   flex-direction: row;
 
-  align-self: ${({ type }) => (type === "PRIMARY" ? "" : "center")};
+  align-self: ${({ size }) => (size === "PRIMARY" ? "" : "center")};
   align-items: center;
   justify-content: center;
 
   padding: 16px 24px;
   gap: 12px;
 
+  border: 1px solid ${({ theme }) => theme.COLORS.GRAY_700};
   border-radius: 6px;
 `;
 
-export const Icon = styled(MaterialIcons)`
-  color: ${({ theme }) => theme.COLORS.WHITE};
+export const Icon = styled(MaterialIcons)<IconProps>`
+  color: ${({ theme, type }) =>
+    type === "PRIMARY" ? theme.COLORS.WHITE : theme.COLORS.GRAY_700};
 `;
 
-export const Text = styled.Text`
-  color: ${({ theme }) => theme.COLORS.WHITE};
+export const Text = styled.Text<ButtonProps>`
+  color: ${({ theme, type }) =>
+    type === "PRIMARY" ? theme.COLORS.WHITE : theme.COLORS.GRAY_700};
+
   font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
   font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
 `;
